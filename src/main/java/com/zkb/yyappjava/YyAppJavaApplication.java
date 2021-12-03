d0825040f701849153dd09dba4d25bbde8c717d7
zhengkb <zkb2668977574@163.com>
2021-12-03 15:33:58 +0800

    1.异影app后端

---
+++ src/main/java/com/zkb/yyappjava/YyAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zkb.yyappjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyAppJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyAppJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkb/yyappjava/YyAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkb.yyappjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyAppJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
